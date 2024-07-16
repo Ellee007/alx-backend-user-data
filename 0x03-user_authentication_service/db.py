adc1496ede8f9c38c829203bb66185f6ca26ce69
Ellee007 <alhassanaliyu83@gmail.com>
2024-07-16 14:03:02 -0700

    file 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-
+from typing import TypeVar
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
+import bcrypt
 
 
 class DB:
@@ -17,7 +20,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,36 +35,43 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add new user
+        """ Adds a user to the database
+        Return: User object
         """
-        user = User(email=email, _password=hashed_password)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """ Find user
+        """ Find a user by a key-value pair
+        Return: The first row of the search result
         """
+        if not kwargs:
+            raise InvalidRequestError
+
+        columns = User.__table__.columns.keys()
+
+        for key in kwargs.keys():
+            if key not in columns:
+                raise InvalidRequestError
+
         user = self._session.query(User).filter_by(**kwargs).first()
+
         if user is None:
-            raise NoResultFound()
+            raise NoResultFound
+
         return user
 
-    def update_user(self, user: User, **kwargs) -> None:
-        """ Update user
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ A method to update user in database
         """
-        for key, value in kwargs.items():
-            setattr(user, key, value)
-        self._session.commit()
-
-    def remove(self, obj: object) -> None:
-        """ Remove object
-        """
-        self._session.delete(obj)
-        self._session.commit()
-
-    def reload(self) -> None:
-        """ Reload database
-        """
-        Base.metadata.create_all(self._engine)
-        self.__session = None
+        columns = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in columns:
+                raise ValueError
+        user = self._session.query(User).filter_by(id=user_id).first()
+        if user:
+            for k, v in kwargs.items():
+                setattr(user, k, v)
